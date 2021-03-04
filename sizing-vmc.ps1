41f6ba93bbf72a0a0457d86c63fc9d8ddf08c038
Adam Bohle <bohleadam@gmail.com>
2021-03-04 14:56:29 +0000

    license and notice updates

--- sizing-vmc.ps1
+++ sizing-vmc.ps1
@@ -1,3 +1,10 @@
+<#VMware Cloud on AWS Sizing PowerShell Script
+Copyright 2021 VMware, Inc.
+
+This product is licensed to you under the BSD-2 license (the "License"). You may not use this product except in compliance with the BSD-2 License.  
+
+This product may include a number of subcomponents with separate copyright notices and license terms. Your use of these subcomponents is subject to the terms and conditions of the subcomponent's license, as noted in the LICENSE file.#> 
+
 param (
     [Parameter(Position=0,mandatory=$true,HelpMessage="Enter the vCenter FQDN or IP address which you would like to gather sizing information from.")]
     [string] $vcenterfqdn, 
